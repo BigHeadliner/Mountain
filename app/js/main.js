ba3595a0cc5d81e2c1d192f69aeec8c3aa9317e1
BigHeadliner <bigheadliner@gmail.com>
2022-08-22 13:35:16 +0300

    fonts conect correctly and vars written right

--- app/js/main.js
+++ app/js/main.js
@@ -68,4 +68,20 @@ $('.history-photo__slider').slick({
       },
 
      ]
-})
+}) 
+ 
+$(document).ready(function(){
+  $(".menu a ").on("click", function (event) {
+    //відміняєм стандартне опрацювання натиску ссилки
+    event.preventDefault();
+     
+    //забираєм ідентифікатор блоку з атрибута href 
+    var id  = $(this).attr('href'),
+
+    //дізнаємся висоту від початку сторінки до блоку на який ссилається якір
+      top = $(id).offset().top;
+    
+    //анімуєм перехід на відстань - top за 1500 мс
+    $('body,html').animate({scrollTop: top}, 1500);
+  });
+});
